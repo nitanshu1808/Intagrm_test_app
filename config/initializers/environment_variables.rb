6f7683c0fc7620ab16bd04a65a45ce0ed180cf8a
nitanshu1808 <nitanshu.rehani@kiwitech.com>
2018-02-25 22:19:54 +0000

    changes in the envioronment variable

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -4,7 +4,7 @@ module EnvironmentVariablesExample
       env_file = Rails.root.join("config", 'local_env.yml').to_s
 
       if File.exists?(env_file)
-        YAML.load_file(env_file)[Rails.env].select{|key, val| ENV[key.to_s] = val}
+        YAML.load_file(env_file)[Rails.env] && YAML.load_file(env_file)[Rails.env].select{|key, val| ENV[key.to_s] = val}
       end # end if File.exists?
     end # end config.before_configuration
   end # end class
