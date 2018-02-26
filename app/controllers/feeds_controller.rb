f7b1750612c749230fd717d43f2e3a067bbcd34b
nitanshu1808 <nitanshu.rehani@kiwitech.com>
2018-02-26 17:33:11 +0000

    displaying images of user

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,4 +1,20 @@
 class FeedsController < ApplicationController
   def index
+    if current_user
+      begin
+        # response = RestClient.get "https://api.instagram.com/v1/users/self/?access_token=", {:params => { access_token:  current_user.oauth_token}}
+        response = RestClient.get ENV["INSTAGRAM_URL"] + ENV["MEDIA_URL"] + "?access_token=", {:params => { access_token:  current_user.oauth_token}}
+        @results = JSON.parse(response)
+        @media   = @results["data"]
+      rescue RestClient::BadRequest => error
+        retries ||= 0
+        if retries == 0
+          retries = 1
+          retry
+        else
+          raise error
+        end
+      end
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fetch_image_url(media)
+    media && media["images"] && media["images"]["standard_resolution"] && media["images"]["standard_resolution"]["url"]
+  end
+
+  def fetch_caption(media)
+    media && media["caption"] && media["caption"]["text"]
+  end
 end
