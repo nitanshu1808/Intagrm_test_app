207d702db9811cf4f7f98158d9a9b585b71a9bc5
nitanshu1808 <nitanshu.rehani@kiwitech.com>
2018-02-27 23:18:05 +0000

    Added pagination and css

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,10 +2,10 @@ class FeedsController < ApplicationController
   def index
     if current_user
       begin
-        # response = RestClient.get "https://api.instagram.com/v1/users/self/?access_token=", {:params => { access_token:  current_user.oauth_token}}
-        response = RestClient.get ENV["INSTAGRAM_URL"] + ENV["MEDIA_URL"] + "?access_token=", {:params => { access_token:  current_user.oauth_token}}
-        @results = JSON.parse(response)
-        @media   = @results["data"]
+        response = RestClient.get ENV["INSTAGRAM_URL"] + ENV["MEDIA_URL"] + "?access_token=", {:params => { access_token:  current_user.oauth_token,max_id: params["max_id"], count: 9}}
+        results  = JSON.parse(response)
+        @max_id  = results["pagination"] && results["pagination"]["next_max_id"]
+        @media   = results["data"]
       rescue RestClient::BadRequest => error
         retries ||= 0
         if retries == 0
